2bf18ba30ee6c8f102194a257895f21c9408195e
C4mz0r <aut0x@hotmail.com>
2015-10-13 12:05:32 -0600

    determine parent file on startup and allow client to filter

--- server.rb
+++ server.rb
@@ -1,36 +1,55 @@
 require 'socket'
+require 'colorize'
 CRLF = "\r\n"
+FRAMEFILE = "picture.html"
+SERVERROOT = File.expand_path(File.dirname(__FILE__))
+$filter = ""
 
-# Respond to a request
-# Returns the response
-def respond_to_request(request)
+
+def respond_to_request(request, folder="")
 	response = ""
 	
-	begin
-		puts "Received request: #{request}"
-		desired_folder = parse_folder_from_request(request)
-		puts "Searching for random image file"	
-		#Example:  body = read_file(select_random_image("/home/me/Desktop"))
-		body = read_file(select_random_image(desired_folder))		
+	split_request = request.split
+
+	if split_request[0] != "GET"
+		puts "Unsupported command.  This server only supports GET commands"
+		return
+	end
+		
+	puts "Found #{split_request[1]}".colorize(:blue)	
+
+	if split_request[1].include? FRAMEFILE
+
+		Dir.chdir SERVERROOT
+		body = read_file(FRAMEFILE)		
 		response += "HTTP/1.0 200 OK" + CRLF
-		response += "Content-Type: image/jpeg" + CRLF		
-		response += "Content-Length: #{body.length}" + CRLF
-		response += CRLF # HTTP convention is a blank line between headers and body
+		response += "Content-Type: text/html" + CRLF
+		response += CRLF
+		response += body		
+		puts "Going to return the picture.html".colorize(:red)
+		$filter = split_request[1].split('?')[1]
+		puts "Got #{$filter}"
+		return response
+
+	else
+		#filter = "c4m" #split_request[1]#.split("|")
+		#puts "Will send a filter of #{filter}".colorize(:red)
+	
+		#puts "Will try to search in #{folder}".colorize(:green)
+		Dir.chdir SERVERROOT
+		puts "Dir is #{Dir.pwd}".colorize(:yellow)
+		random_file = select_random_image(folder, $filter)
+		puts "Trying to return #{random_file}".colorize(:green)
+		body = ""
+		body = read_file(random_file) if random_file != nil				
+		response += "HTTP/1.0 200 OK" + CRLF
+		response += "Content-Type: text/jpeg" + CRLF
+		response += CRLF
 		response += body
-	rescue
-		puts "Could not find any item"
-		response = "HTTP/1.0 404 Not Found" + CRLF
+		puts "Going to return the picture.html".colorize(:red)
+		return response
 	end
 
-	return response
-end
-
-def parse_folder_from_request(request)
-	puts "#{request[0]}"#.colorize(:green)
-	folder = request.match('.*f=(.*) HTTP')[1]
-	puts "Found folder = #{folder}"#.colorize(:red)
-	raise if folder.nil?
-	folder
 end
 
 # Reads a file into a variable for return to requestor
@@ -45,12 +64,25 @@ def read_file(item)
 	content
 end
 
-def select_random_image(folder)
+def select_random_image(folder, filter="")
 	Dir.chdir folder
-	Dir.glob("**/*").select { |f| f.end_with?(".jpg") }.sample
+	selected_file = ""
+	if !filter.nil?
+		selected_file = Dir.glob("**/*").select { |f| f.end_with?(".jpg") and f.include?(filter) }.sample
+	else
+		selected_file = Dir.glob("**/*").select { |f| f.end_with?(".jpg") }.sample
+	end
+	puts "The random image is #{selected_file}".colorize(:orange)
+	return selected_file
 end
 
-server = TCPServer.open(2000)					# listen on port 2000 for client to connect
+puts "Enter the server IP address (or localhost if just using locally)"
+ip_address = gets.chomp
+ip_address = "localhost" if ip_address.empty?
+puts "Enter the directory to limit searches within"
+directory  = gets.chomp
+
+server = TCPServer.open(ip_address, 2000)					# listen on port 2000 for client to connect
 loop do
 	client = server.accept					# wait for a client to connect
 	request = ""
@@ -61,7 +93,7 @@ loop do
 		end
 	end
 
-	response = respond_to_request(request)		# parse out the request and respond to it (body is optional)
+	response = respond_to_request(request, directory)		# parse out the request and respond to it (body is optional)
 	client.puts response
 	client.close
 end
